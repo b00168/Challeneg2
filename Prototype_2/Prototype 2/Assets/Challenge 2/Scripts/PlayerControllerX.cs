0f180cd69e7f0469a852b30e552ee64f390545f2
Stephen Smyth <stephenbsmyth28@gmail.com>
2024-10-08 15:41:28 +0100

    Challenge2

---
+++ Prototype_2/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float time = 2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (time >=2f))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            time = 0f;
+        }
+    }
+}
